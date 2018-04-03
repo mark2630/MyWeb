2bd726ca47a1cf7283d4b1adfcff95ec613349f0
Mark Golubev <markg2630@gmail.com>
2018-04-03 00:04:34 -0700

    everything is almost finished, need small fixes and finish skills section

--- scripts.js
+++ scripts.js
@@ -21,7 +21,7 @@ $(window).scroll(function() {
   });
   
   
-/*
+
 // Select all links with hashes
 $('a[href*="#"]')
   // Remove links that don't actually link to anything
@@ -42,21 +42,21 @@ $('a[href*="#"]')
         // Only prevent default if animation is actually gonna happen
         event.preventDefault();
         $('html, body').animate({
-          scrollTop: target.offset().top - ((window.innerHeight - target.height()) / 2)
+          scrollTop: target.offset().top
         }, 1200, function () {
           // Callback after animation
         });
       }
     }
   });
-*/
 
 
+/*
 $(window).scroll(function () {
   $(".hide_on_scroll").css("opacity", 1 -
     $(window).scrollTop() / 400);
 });
-
+*/
 
 $('.bar_text').hide();
 
